7c9776a9a43db03823840f3a55a2f0c7d25e8a86
Tuan Nguyen <Tuan.Nguyen@huntermacdonald.com>
2015-11-11 09:22:10 +0700

    Removed JiraClient test from AppTest

--- crs/crs-jira/src/test/java/org/ihtsdotools/crs/jira/AppTest.java
+++ crs/crs-jira/src/test/java/org/ihtsdotools/crs/jira/AppTest.java
@@ -1,37 +1,8 @@
 package org.ihtsdotools.crs.jira;
 
-import org.ihtsdotools.crs.jira.api.JiraClient;
-import org.ihtsdotools.crs.jira.dto.Issue;
-import org.ihtsdotools.crs.jira.dto.IssueType;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 	
-	JiraClient jiraClient;
-	
-	//@Test
-	public void injectedBeansShouldNotNull() {
-		Assert.assertNotNull(jiraClient);
-	}
-	
-	
-	
-	//@Test
-	public void testCreateIssue() {
-		Issue issue = new Issue();
-		IssueType issueType = new IssueType(10101L, "SCT Content Request Batch", false, "A batch of SCT Content Requests");
-		issue.setIssueType(issueType);
-		issue.setProjectKey("CRT");
-		issue.setSummary("This issue is created for testing");
-		issue.setDescription("This issue is created by Unit Test of CRS application");
-		jiraClient.createIssue(issue);
-		jiraClient.close();
-	}
 }
