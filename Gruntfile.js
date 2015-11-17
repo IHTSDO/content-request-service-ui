271b5e26a20f3fa01cce2a7ac38719c0ce50fda9
aflinton@gmail.com <aflinton@gmail.com>
2015-11-17 11:08:19 +0000

    test turn off karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -532,8 +532,8 @@ module.exports = function (grunt) {
         'wiredep',
         'concurrent:test',
         'postcss',
-        'connect:test',
-        'karma'
+        'connect:test'
+        //'karma'
     ]);
 
     grunt.registerTask('build', [
